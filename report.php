cbacf5cb5d9c35e74b85340ddf0aad205bc926ec
Sergio Rabellino <sergio.rabellino@di.unito.it>
2026-08-03 15:08:02 +0200

    Fix missing SQL stuffs...

--- report.php
+++ report.php
@@ -147,6 +147,7 @@ class quiz_exportattemptscsv_report extends attempts_report {
      * @param moodle_url $redirecturl where to redircet to after a successful action.
      */
     protected function process_actions($quiz, $cm, $currentgroup, $groupstudents, $allowed, $redirecturl) {
+        global $DB;
         require_capability('quiz/exportattemptscsv:download', $this->context);
         if (empty($currentgroup) || $groupstudents) {
             if (optional_param('export', 0, PARAM_BOOL) && confirm_sesskey()) {
@@ -157,9 +158,10 @@ class quiz_exportattemptscsv_report extends attempts_report {
                     // Keep only attempts that belong to this quiz and to a user this report may show.
                     // IDOR mitigation suggested by MDLShield.
                     [$asql, $aparams] = $DB->get_in_or_equal($attemptids, SQL_PARAMS_NAMED);
+
                     $valid = $DB->get_fieldset_sql(
                         "SELECT quiza.id
-                           FROM {quiz_attempts} quiza
+                           FROM {quiz_attempts} quiza, {user} u
                            {$allowed->joins}
                           WHERE quiza.quiz = :quizid
                             AND quiza.id $asql
@@ -311,7 +313,7 @@ class quiz_exportattemptscsv_report extends attempts_report {
             [$asql, $aparams] = $DB->get_in_or_equal($attemptid, SQL_PARAMS_NAMED);
             $valid = $DB->get_fieldset_sql(
                 "SELECT quiza.id
-                   FROM {quiz_attempts} quiza
+                   FROM {quiz_attempts} quiza, {user} u
                    {$allowed->joins}
                   WHERE quiza.quiz = :quizid
                     AND quiza.id $asql
